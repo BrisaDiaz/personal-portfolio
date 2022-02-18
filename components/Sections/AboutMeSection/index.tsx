dac8b61d6913302208d595f54da4b3214d4e24f5
Brisa Diaz <brisaabigaildiaz2000@gmail.com>
2022-02-17 23:53:37 -0300

    adding contact button to main page

--- components/Sections/AboutMeSection/index.tsx
+++ components/Sections/AboutMeSection/index.tsx
@@ -1,5 +1,6 @@
 import Illustration from "@/components/Sections/Illustration";
-
+import Button from "@/components/Button";
+import Link from "next/link";
 export default function AboutMeSection({ styles }: { styles: any }) {
   return (
     <section className={`container ${styles.aboutMeSection}`} id="/">
@@ -10,13 +11,7 @@ export default function AboutMeSection({ styles }: { styles: any }) {
             <br />
             {"I'm a Frontend Developer"}
           </h1>
-          <p>
-            I describe myself as a proactive, curious and determined person,
-            passionate of providing solutions in a creative and ingenious way
-            and {"don't "}hesitate to leave the comfort zone to face new
-            challenges, seeing each of them as an opportunity to improve and
-            bring value.
-          </p>
+
           <p>
             I have almost two years of experience working on different kinds of
             projects like websites and web applications, working with the most
@@ -24,6 +19,19 @@ export default function AboutMeSection({ styles }: { styles: any }) {
             the user experience, development and scalability of digital
             products.
           </p>
+          <p>
+            I describe myself as a proactive, curious and determined person,
+            passionate about providing solutions in a creative and ingenious way
+            who {"don't "}hesitate to leave the comfort zone to face new
+            challenges, seeing each of them as an opportunity to improve and
+            bring value.
+          </p>
+          <br />
+          <Link href="/contact" passHref>
+            <a href="" tabIndex={-1}>
+              <Button text="I'd love to hear your proposals!" />
+            </a>
+          </Link>
         </div>
       </div>
       <div className={styles.aboutMeIllustration}>
