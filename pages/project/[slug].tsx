13d6382ecdb48cb55f81989e7dadec8b3d967f50
Brisa Diaz <brisaabigaildiaz2000@gmail.com>
2022-03-03 00:50:51 -0300

    fixing project page layout overlap

--- pages/project/[slug].tsx
+++ pages/project/[slug].tsx
@@ -41,136 +41,139 @@ const ProjectPage: NextPage<{ project: Project; notFound?: boolean }> = ({
         <title>Brisa Díaz | {project.title}</title>
       </Head>
       <WithNavbar>
-        <main className={`main ${styles["page"]}`}>
-          <SocialShareButtons />
+        <>
+          <main className="main">
+            <section className={`container ${styles["page"]}`}>
+              {project?.subtitle && (
+                <h1 className={`title2 ${styles["page__title"]}`}>
+                  {project?.subtitle}
+                </h1>
+              )}
+              <div className={styles["page__content-container"]}>
+                <section className={styles["carousel-container"]}>
+                  <Carousel
+                    onZoom={openImageBackdrop}
+                    captions={project?.captions?.slice(1) || []}
+                    objectFit="cover"
+                    width={1200}
+                    height={500}
+                    thumbnail={project?.captions[0]?.src || ""}
+                  />
+                </section>
+                <section>
+                  {project?.testingUser ? (
+                    <>
+                      <h2 className={`title3 ${styles["page__subtitle"]}`}>
+                        Credentials
+                      </h2>
+                      <div className={styles.credentials}>
+                        <p className={styles["credentials__item"]}>
+                          <b className={`${styles.label}`}>Email:</b>
+                          {project?.testingUser?.email}
+                        </p>
+                        <p className={styles["credentials__item"]}>
+                          <b className={`${styles.label}`}>Password:</b>
+                          {project?.testingUser?.password}
+                        </p>
+                      </div>
+                    </>
+                  ) : null}
+                  <h2 className={`title3 ${styles["page__subtitle"]}`}>
+                    Links
+                  </h2>
+                  <div className={styles["project-links"]}>
+                    <a
+                      href={project?.demo}
+                      target="_blank"
+                      rel="noreferrer"
+                      title="Link to live demo"
+                      className={styles["project-links__link"]}
+                    >
+                      <SVG name="website-fill" width={20} height={20} />
+                      Live Demo
+                    </a>
 
-          <section className={`container ${styles.container}`}>
-            <ParticlesBackground />
-            {project?.subtitle && (
-              <h1 className={`title2 ${styles["page__title"]}`}>
-                {project?.subtitle}
-              </h1>
-            )}
-            <div className={styles["page__content-container"]}>
-              <section className={styles["carousel-container"]}>
-                <Carousel
-                  onZoom={openImageBackdrop}
-                  captions={project?.captions?.slice(1) || []}
-                  objectFit="cover"
-                  width={1200}
-                  height={500}
-                  thumbnail={project?.captions[0]?.src || ""}
-                />
-              </section>
-              <section>
-                {project?.testingUser ? (
-                  <>
-                    <h2 className={`title3 ${styles["page__subtitle"]}`}>
-                      Credentials
-                    </h2>
-                    <div className={styles.credentials}>
-                      <p className={styles["credentials__item"]}>
-                        <b className={`${styles.label}`}>Email:</b>
-                        {project?.testingUser?.email}
-                      </p>
-                      <p className={styles["credentials__item"]}>
-                        <b className={`${styles.label}`}>Password:</b>
-                        {project?.testingUser?.password}
-                      </p>
-                    </div>
-                  </>
-                ) : null}
-                <h2 className={`title3 ${styles["page__subtitle"]}`}>Links</h2>
-                <div className={styles["project-links"]}>
-                  <a
-                    href={project?.demo}
-                    target="_blank"
-                    rel="noreferrer"
-                    title="Link to live demo"
-                    className={styles["project-links__link"]}
-                  >
-                    <SVG name="website-fill" width={20} height={20} />
-                    Live Demo
-                  </a>
+                    <a
+                      href={project?.source_code}
+                      target="_blank"
+                      rel="noreferrer"
+                      title="Link to source code"
+                      className={styles["project-links__link"]}
+                    >
+                      <SVG name="code-fill" width={20} height={20} />
+                      Source Code
+                    </a>
+                  </div>
 
-                  <a
-                    href={project?.source_code}
-                    target="_blank"
-                    rel="noreferrer"
-                    title="Link to source code"
-                    className={styles["project-links__link"]}
-                  >
-                    <SVG name="code-fill" width={20} height={20} />
-                    Source Code
-                  </a>
-                </div>
-
-                <h2 className={`title3 ${styles["page__subtitle"]}`}>
-                  Technologies
-                </h2>
-                <p className={styles["tech-list"]}>
-                  <b
-                    className={`${styles.label} ${styles["tech-list__legend"]}`}
-                  >
-                    Language:
-                  </b>
-                  <span className={styles["tech-list__chip"]}>
-                    {project?.language}
-                  </span>
-                </p>
-                <p className={styles["tech-list"]}>
-                  <b
-                    className={`${styles.label} ${styles["tech-list__legend"]}`}
-                  >
-                    Hosting:
-                  </b>
-                  <span className={styles["tech-list__chip"]}>
-                    {project?.technologies?.hosting}
-                  </span>
-                </p>
-
-                <p className={styles["tech-list"]}>
-                  <b
-                    className={`${styles.label} ${styles["tech-list__legend"]}`}
-                  >
-                    Frontend:
-                  </b>
-                  {project?.technologies?.frontend.map((tech: string) => (
-                    <span key={tech} className={styles["tech-list__chip"]}>
-                      {tech}
-                    </span>
-                  ))}
-                </p>
-                {project?.technologies?.backend ? (
+                  <h2 className={`title3 ${styles["page__subtitle"]}`}>
+                    Technologies
+                  </h2>
                   <p className={styles["tech-list"]}>
                     <b
                       className={`${styles.label} ${styles["tech-list__legend"]}`}
                     >
-                      Backend:
+                      Language:
                     </b>
-                    {project?.technologies?.backend?.map((tech: string) => (
-                      <span className={styles["tech-list__chip"]} key={tech}>
-                        {tech}
-                      </span>
-                    ))}
+                    <span className={styles["tech-list__chip"]}>
+                      {project?.language}
+                    </span>
                   </p>
-                ) : null}
-                {project?.technologies?.testing ? (
                   <p className={styles["tech-list"]}>
-                    <b className={`${styles.label}`}>Testing:</b>
-                    {project?.technologies?.testing?.map((tech: string) => (
-                      <span className={styles["tech-list__chip"]} key={tech}>
+                    <b
+                      className={`${styles.label} ${styles["tech-list__legend"]}`}
+                    >
+                      Hosting:
+                    </b>
+                    <span className={styles["tech-list__chip"]}>
+                      {project?.technologies?.hosting}
+                    </span>
+                  </p>
+
+                  <p className={styles["tech-list"]}>
+                    <b
+                      className={`${styles.label} ${styles["tech-list__legend"]}`}
+                    >
+                      Frontend:
+                    </b>
+                    {project?.technologies?.frontend.map((tech: string) => (
+                      <span key={tech} className={styles["tech-list__chip"]}>
                         {tech}
                       </span>
                     ))}
                   </p>
-                ) : null}
-              </section>
-            </div>
-          </section>
+                  {project?.technologies?.backend ? (
+                    <p className={styles["tech-list"]}>
+                      <b
+                        className={`${styles.label} ${styles["tech-list__legend"]}`}
+                      >
+                        Backend:
+                      </b>
+                      {project?.technologies?.backend?.map((tech: string) => (
+                        <span className={styles["tech-list__chip"]} key={tech}>
+                          {tech}
+                        </span>
+                      ))}
+                    </p>
+                  ) : null}
+                  {project?.technologies?.testing ? (
+                    <p className={styles["tech-list"]}>
+                      <b className={`${styles.label}`}>Testing:</b>
+                      {project?.technologies?.testing?.map((tech: string) => (
+                        <span className={styles["tech-list__chip"]} key={tech}>
+                          {tech}
+                        </span>
+                      ))}
+                    </p>
+                  ) : null}
+                </section>
+              </div>
+            </section>
 
-          <ImageBackdrop {...backdropState} onClose={closeBackdrop} />
-        </main>
+            <ImageBackdrop {...backdropState} onClose={closeBackdrop} />
+          </main>
+          <SocialShareButtons />
+          <ParticlesBackground />
+        </>
       </WithNavbar>
     </div>
   );
